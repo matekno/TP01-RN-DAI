b547eaeb3446d9e3b7ea41260aed9f441b2f71ad
matekno <mati.rapoport@gmail.com>
2022-08-08 08:36:25 -0300

    initail

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert} from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = useState('')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.head1}>Open up App.js to start working on your app!</Text>
+      {/*No entendo porque hay que poner value=text en el textinput */}
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={"Ingrese su texto"}
+      />
+      <Button
+        title='Polshu, tocame!'
+        color="#f185dd"
+        onPress={() => Alert.alert("ola polshu esta es mi primera pagina web")}>
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +31,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  head1: {
+    backgroundColor: '#f000ff' 
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
+  }
 });
