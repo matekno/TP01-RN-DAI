b9041eb4016692b8de173166277b536cd9015b20
matekno <mati.rapoport@gmail.com>
2022-08-08 09:39:47 -0300

    app finalizada

--- App.js
+++ App.js
@@ -1,43 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Alert} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 export default function App() {
   const [text, onChangeText] = useState('')
   return (
-    <View style={styles.container}>
-      <Text style={styles.head1}>Open up App.js to start working on your app!</Text>
-      {/*No entendo porque hay que poner value=text en el textinput */}
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder={"Ingrese su texto"}
-      />
-      <Button
-        title='Polshu, tocame!'
-        color="#f185dd"
-        onPress={() => Alert.alert("ola polshu esta es mi primera pagina web")}>
-      </Button>
-      <StatusBar style="auto" />
+    <View style={styles.main}>
+      <View style={[styles.container, { flexDirection: "column" }]}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder={"Ingrese su texto"}
+        />
+        <Button
+          style={styles.button}
+          title='Polshu, tocame!'
+          color="#f185dd"
+          onPress={() => Alert.alert("ola polshu esta es mi primera pagina web")}>
+        </Button>
+        <View style={styles.lineStyle} />
+      
+        <View style={styles.respuesta}>
+          <Text>{text}</Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    width: "70%",
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff0f',
+  },
   head1: {
-    backgroundColor: '#f000ff' 
+    backgroundColor: '#f000ff'
   },
   input: {
+    width: "100%",
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10
+  },
+  button: {
+    flex: 2,
+    width: "100%"
+  },
+  lineStyle: {
+    borderWidth: 0.6,
+    width: "100%",
+    borderColor: 'black',
+    marginTop: 10,
+  },
+  respuesta: {
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
